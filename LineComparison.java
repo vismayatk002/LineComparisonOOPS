91099f3019171bf12e048dfc7b80107f97c8e274
vismayatk002 <vismayatk002@gmail.com>
2021-08-04 23:20:34 +0530

    Refactor : check equality of two lines

--- LineComparison.java
+++ LineComparison.java
@@ -1,9 +1,21 @@
 import java.lang.Math;
+import java.lang.*;
 public class LineComparison{
 
     public double calcLength(int x1 ,int y1 ,int x2 ,int y2){
 
-        double length =  Math.sqrt( Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2) );
+        // Calculate length of first Line 
+        Double length =  Math.sqrt( Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2) );
         return length;
+    }
+    public boolean checkEqual(Double length1 , Double length2){
+        
+        // Check equality of first and second line
+        if(length1.equals(length2)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
--- LineComparisonMain.java
+++ LineComparisonMain.java
@@ -3,21 +3,48 @@ public class LineComparisonMain{
 
     public static void main(String[] args){
 
+        int x1 ,y1 , x2 , y2;
         Scanner sc= new Scanner(System.in);
         LineComparison line = new LineComparison();
         //Read first co-ordinates
+        System.out.println("Enter co-ordinates of First line"); 
         System.out.println("Enter first co-ordinates (x,y) ");  
         System.out.print("Enter x : ");  
-        int x1= sc.nextInt();  
+        x1= sc.nextInt();  
         System.out.print("Enter y : "); 
-        int y1= sc.nextInt(); 
-
+        y1= sc.nextInt(); 
         //Read second co-ordinates
         System.out.println("Enter second co-ordinates (x,y) ");  
         System.out.print("Enter x : ");  
-        int x2= sc.nextInt();  
+        x2= sc.nextInt();  
         System.out.print("Enter y : "); 
-        int y2= sc.nextInt();
-        System.out.printf("Length of line : %.2f " , line.calcLength(x1 , y1 , x2 , y2));
+        y2= sc.nextInt(); 
+
+        Double length1 = line.calcLength(x1 , y1 , x2 , y2);
+        System.out.printf("Length of first line :%.2f " , length1);
+        
+
+        System.out.println("\nEnter co-ordinates of Second line"); 
+        System.out.println("Enter first co-ordinates (x,y) ");  
+        System.out.print("Enter x : ");  
+        x1= sc.nextInt();  
+        System.out.print("Enter y : "); 
+        y1= sc.nextInt(); 
+        //Read second co-ordinates
+        System.out.println("Enter second co-ordinates (x,y) ");  
+        System.out.print("Enter x : ");  
+        x2= sc.nextInt();  
+        System.out.print("Enter y : "); 
+        y2= sc.nextInt(); 
+
+        Double length2 = line.calcLength(x1 , y1 , x2 , y2);
+        System.out.printf("Length of line : %.2f " , length2);
+
+        if(line.checkEqual(length1 , length2)){
+            System.out.println("\nLines are equal ");
+        }
+        else{
+            System.out.println("\nLines are not equal ");
+        }
     }
 }
